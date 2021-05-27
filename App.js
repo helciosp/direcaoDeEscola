1a2931c46d0b93f1518f0fe4c9457c9992561fd7
helciosp43 <helciosp43@gmail.com>
2021-05-27 16:19:09 -0300

    Finalizando o Screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Pilhas from './src/routes/Pilhas'
 import { listenOrientationChange as loc, removeOrientationListener as rol } from 'react-native-responsive-screen';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class App extends Component {
   componentDidMount() {
     loc(this);
+    SplashScreen.hide();
   }
 
   componentWillUnMount() {
